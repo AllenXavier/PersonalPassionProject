38ddaeb320d2f65379f7b0b6c6734ce7cb431194
XavierAllen <xavier.allen@student.howest.be>
2019-12-03 14:41:38 +0100

    Added logo and button styling main screen

--- chatty/lib/screens/mainscreen/main_screen.dart
+++ chatty/lib/screens/mainscreen/main_screen.dart
@@ -5,107 +5,124 @@ class mainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              stops: [0.1, 0.4, 0.7, 0.9],
-              colors: [
-                Color(0xFF3594DD),
-                Color(0xFF4563DB),
-                Color(0xFF5036D5),
-                Color(0xFF5B16D0),
-              ],
+      body:
+        Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [0.1, 0.4, 0.7, 0.9],
+                  colors: [
+                    Color(0xFF3594DD),
+                    Color(0xFF4563DB),
+                    Color(0xFF5036D5),
+                    Color(0xFF5B16D0),
+                  ],
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: showLogo(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Text('Join or become a host.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: Container(
+                      width: 220.0,
+                      child: FlatButton(
+                        textColor: Colors.white,
+                        color: Colors.white30,
+                        onPressed: () {
+                          // TODO
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                'Become host',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24.0,
+                                ),
+                              ),
+                            ),
+                            Icon(
+                              Icons.wifi,
+                              color: Colors.white,
+                              size: 32,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Container(
+                      width: 220.0,
+                      child: FlatButton(
+                        textColor: Colors.white,
+                        color: Colors.white30,
+                        onPressed: () {
+                          // TODO
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                'Join chat',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24.0,
+                                ),
+                              ),
+                            ),
+                            Icon(
+                              Icons.message,
+                              color: Colors.white,
+                              size: 32,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Text('Join or become a host',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Container(
-                  width: 220.0,
-                  child: FlatButton(
-                    textColor: Colors.white,
-                    color: Colors.white30,
-                    onPressed: () {
-                      // TODO
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            'Become host',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24.0,
-                            ),
-                          ),
-                        ),
-                        Icon(
-                          Icons.account_circle,
-                          color: Colors.white,
-                          size: 32,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Container(
-                  width: 220.0,
-                  child: FlatButton(
-                    textColor: Colors.white,
-                    color: Colors.white30,
-                    onPressed: () {
-                      // TODO
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            'Join chat',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24.0,
-                            ),
-                          ),
-                        ),
-                        Icon(
-                          Icons.message,
-                          color: Colors.white,
-                          size: 32,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
+}
+
+Widget showLogo() {
+  return new Hero(
+    tag: 'hero',
+    child: Padding(
+      padding: EdgeInsets.all(32.0),
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/images/chatty_logo.png'),
+      ),
+    ),
+  );
 }
